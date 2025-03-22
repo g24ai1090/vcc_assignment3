46429cf7f7809d1c430c301cda3b8fd1a33d2aea
g24ai1090 <g24ai1090@iitj.ac.in>
2025-03-22 13:43:16 +0530

    file updated

--- server.js
+++ server.js
@@ -51,5 +51,9 @@ app.delete('/customers/:id', (req, res) => {
     res.json({ message: 'Customer deleted' });
 });
 
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 exports.customerAPI = app;
